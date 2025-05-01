ea287acd0230295780ae94472069923c065b7093
wan <shooky0510@gmail.com>
2025-05-01 18:49:42 +0900

    add

---
+++ ec2/main.tf
@@ -0,0 +1,46 @@
+data "aws_region" "current" {}
+
+resource "aws_vpc" "main" {
+  cidr_block           = "192.168.0.0/22"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+}
+
+resource "aws_subnet" "private" {
+  availability_zone = "${data.aws_region.current.name}a"
+  cidr_block        = "192.168.1.0/24"
+  vpc_id            = aws_vpc.main.id
+}
+
+resource "aws_subnet" "public" {
+  availability_zone = "${data.aws_region.current.name}a"
+  cidr_block        = "192.168.2.0/24"
+  vpc_id            = aws_vpc.main.id
+}
+
+# resource "aws_instance" "main" {
+#   ami           = "ami-0e449927258d45bc4"
+#   instance_type = "t2.micro"
+#   subnet_id     = aws_subnet.private.id
+#   # key_name                    = "demo-key"
+#   associate_public_ip_address = false
+#   vpc_security_group_ids      = [aws_security_group.ec2.id]
+#   availability_zone           = "${data.aws_region.current.name}a"
+#   iam_instance_profile        = aws_iam_instance_profile.systems_manager.name
+# }
+
+# resource "aws_security_group" "ec2" {
+#   name        = "ec2-sg"
+#   vpc_id      = aws_vpc.main.id
+#   description = "ec2 security group"
+# }
+
+# # ステップ３で作ったEC2のファイルの続き
+
+# resource "aws_vpc_security_group_egress_rule" "ec2_egress" {
+#   from_port         = 443
+#   to_port           = 443
+#   ip_protocol       = "tcp"
+#   security_group_id = aws_security_group.ec2.id
+#   cidr_ipv4         = local.cidr_blocks.private
+# }
